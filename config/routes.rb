27431cb7dc3acf9b9b54d33ea4b3d5cb36c8df8c
Yetunde Alese <yaalese1@gmail.com>
2023-02-07 21:53:02 -0500

    env help

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :bookings
+  resources :bookings ejflje
   resources :pets
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
